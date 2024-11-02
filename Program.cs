8dedcf23082c2821bf67aaa727fb070777a1dd1b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-01 22:34:22 -0300

    Etanol e gasolina na sala...

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿decimal gasolina, etanol, razao;  
+
+Console.Clear(); 
+
+Console.WriteLine("--- Etanol ou gasolina? ---\n");
+Console.Write("Preço do etanol (R$).....: ");
+etanol = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Preço da gasolina (R$).....: ");
+gasolina = Convert.ToDecimal(Console.ReadLine());
+
+razao = RazaoEtanolGasolina(etanol, gasolina);
+
+Console.WriteLine($"\nO preço do etanol corresponde a {razao:P1} do preço da gasolina.");
+Console.WriteLine($"\nRecomendação: Abasteça com {(AbastecerComGasolina(razao) ? "GASOLINA" : "ETANOL")}.");
+
+
+decimal RazaoEtanolGasolina(decimal valorEtanol, decimal valorGasolina)
+{
+    return valorEtanol / valorGasolina;
+}
+
+bool AbastecerComGasolina(decimal razao)
+{
+   const decimal percentualIdeal = 0.73M;
+    return razao > percentualIdeal;
+}
